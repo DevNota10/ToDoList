65f83e4407affdf06b5d5befb84d3513b64ca76d
Kleber <dev.nota10.k@gmail.com>
2023-05-27 21:39:08 -0300

    Projeto Do Do List

---
+++ Reademe.md
@@ -0,0 +1,2 @@
+Projeto elaborado por Rodolfo Mori.
+Dispponível no link : https://www.youtube.com/watch?v=k0roUpojoSE&t=1651s
---
+++ script/script.js
@@ -0,0 +1,62 @@
+
+    const btnTask = document.querySelector(".btn-add-task");
+    const inputTask = document.querySelector(".input-task");
+    const listTasks = document.querySelector(".list-tasks");
+
+    myList = [];
+
+    function addNewTask(){
+        myList.push({
+           task: inputTask.value,
+           completed: false,
+
+
+        });
+        inputTask.value = '';
+
+        showTask();
+    }
+
+    function showTask (){
+      
+        let newLi = '';
+
+        myList.forEach((item,index) =>{
+            newLi =
+             newLi +
+            `  
+             <li class="task ${item.completed && "done" }">
+                <img  src="./img/checked.png" alt="Img-check" onclick="completeTask(${index})"/>
+                <p>${item.task}</p>
+                
+                <img src="./img/trash.png" alt="Excluir-task" onclick="deleteItem(${index})">
+              </li>
+            `
+        });
+        listTasks.innerHTML = newLi
+        localStorage.setItem('list', JSON.stringify(myList))
+
+    };  
+
+    function completeTask(index){
+        myList[index].completed = !myList[index].completed
+        showTask()
+    };
+
+    function deleteItem(index){
+        myList.splice(index,1);
+        showTask();
+    };
+
+    function reloadTasks(){
+        const myTaskLocalStorage = localStorage.getItem('list')
+
+        if(myTaskLocalStorage){
+            myList = JSON.parse(myTaskLocalStorage );
+        }
+        showTask();
+    };
+
+    reloadTasks();
+    btnTask.addEventListener("click", addNewTask);
+
